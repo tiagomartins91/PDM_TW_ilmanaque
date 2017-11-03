dfb9c8ee803a5f1b7a88c2da79ddc2598d570db8
Tiago Martins <TiagoMartins@Tiagos-MacBook-Pro.local>
2017-11-03 22:16:18 +0000

    Initial commit

---
+++ app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/MainActivity.java
@@ -0,0 +1,13 @@
+package pdm.di.ubi.pt.tw_ilmanaque;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
