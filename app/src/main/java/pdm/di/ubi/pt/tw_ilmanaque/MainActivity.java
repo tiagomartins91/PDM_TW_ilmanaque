6d94bc58a38e3b017a8b950a60d62012df2694ac
saraiva <joaowowsaraiva@gmail.com>
2017-12-13 20:25:16 +0000

    nd de  jeito

--- app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/MainActivity.java
+++ app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //teste coment
+    //teste bora la comeºar meus utssss<
+
+    //aiuuuuuuuuuuuuuuuuuuuuui
 }
