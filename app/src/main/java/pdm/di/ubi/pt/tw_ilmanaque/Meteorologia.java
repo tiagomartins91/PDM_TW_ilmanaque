1631c288d3aefbae8eb5bc8e004d64b9fa686475
TiagoMartins <tiago_alex_martins@hotmail.com>
2017-12-15 23:12:28 +0000

    Teste

---
+++ app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/Meteorologia.java
@@ -0,0 +1,20 @@
+package pdm.di.ubi.pt.tw_ilmanaque;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TiagoMartins on 15/12/2017.
+ */
+
+public class Meteorologia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.meteorologia);
+    }
+
+
+
+}
