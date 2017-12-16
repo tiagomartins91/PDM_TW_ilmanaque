da05d889d6d3aa991c2df7d5ea22c8f19621315c
TiagoMartins <tiago_alex_martins@hotmail.com>
2017-12-16 01:14:36 +0000

    BD e Registar Atividades

--- app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/MainActivity.java
+++ app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/MainActivity.java
@@ -1,16 +1,21 @@
 package pdm.di.ubi.pt.tw_ilmanaque;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity{
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
--- app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/RegistoAtividades.java
+++ app/src/main/java/pdm/di/ubi/pt/tw_ilmanaque/RegistoAtividades.java
@@ -1,7 +1,14 @@
 package pdm.di.ubi.pt.tw_ilmanaque;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Created by TiagoMartins on 15/12/2017.
@@ -9,11 +16,54 @@ import android.support.v7.app.AppCompatActivity;
 
 public class RegistoAtividades extends AppCompatActivity {
 
+
+
+    EditText nomeplanta, terreno, quantidade, data;
+    Button registar;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registoatividades);
+
+        nomeplanta = (EditText) findViewById(R.id.etNomePlanta);
+        terreno = (EditText) findViewById(R.id.etTerreno);
+        quantidade= (EditText) findViewById(R.id.etQuantidade);
+        data = (EditText) findViewById(R.id.etData);
+        registar = (Button) findViewById(R.id.bRegistar);
     }
+
+
+
+    public void RegistarAtividade (View v){
+
+        AjudanteBD ajudanteBD = new AjudanteBD(this);
+
+        boolean inserirsucesso = ajudanteBD.inserirDados(
+                nomeplanta.getText().toString(),
+                terreno.getText().toString(),
+                Integer.parseInt(quantidade.getText().toString()),
+                data.getText().toString());
+
+        if (inserirsucesso == true) {
+
+
+            Toast.makeText(this, "Atividade registada com sucesso!", Toast.LENGTH_SHORT).show();
+            Intent menuP = new Intent(this, MainActivity.class);
+            startActivity(menuP);
+
+        }
+         else {
+
+            Toast.makeText(this, "Erro!", Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+
 
 
 
